c583d2a636a45093cb3701b98767aa743da9623f
hugelinkfan15 <158606627+hugelinkfan15@users.noreply.github.com>
2024-09-12 16:32:38 -0500

    begin

---
+++ Pantry Panic/Assets/Scripts/Ingredient.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingredient : MonoBehaviour
+{
+    static private Dictionary<char, KeyCode> keyCodes = new Dictionary<char, KeyCode>  {
+        { 'A', KeyCode.A },
+        { 'B', KeyCode.B },
+        { 'C', KeyCode.C },
+        { 'D', KeyCode.D },
+        { 'E', KeyCode.E },
+        { 'F', KeyCode.F },
+        { 'G', KeyCode.G },
+        { 'H', KeyCode.H },
+        { 'I', KeyCode.I },
+        { 'J', KeyCode.J },
+        { 'K', KeyCode.K },
+        { 'L', KeyCode.L },
+        { 'M', KeyCode.M },
+        { 'N', KeyCode.N },
+        { 'O', KeyCode.O },
+        { 'P', KeyCode.P },
+        { 'Q', KeyCode.Q },
+        { 'R', KeyCode.R },
+        { 'S', KeyCode.S },
+        { 'T', KeyCode.T },
+        { 'U', KeyCode.U },
+        { 'V', KeyCode.V },
+        { 'W', KeyCode.W },
+        { 'X', KeyCode.X },
+        { 'Y', KeyCode.Y },
+        { 'Z', KeyCode.Z } };
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
